aafb8c629764bb4fdc384c9f8752913469f34d8f
Ayush3018 <ayushpp0407@gmail.com>
2023-06-05 13:21:36 +0530

    ok

---
+++ client/src/components/ChatHeader.js
@@ -0,0 +1,26 @@
+import { useCookies } from "react-cookie"
+
+const ChatHeader = ({ user }) => {
+    const [ cookies, setCookie, removeCookie ] = useCookies(['user'])
+
+    const logout = () => {
+        removeCookie('UserId', cookies.UserId)
+        removeCookie('AuthToken', cookies.AuthToken)
+        window.location.reload()
+    }
+
+    return (
+        <div className="chat-container-header">
+            <div className="profile">
+                <div className="img-container">
+                    <img src={''} alt={''}/>
+                </div>
+                <h3>{}</h3>
+            </div>
+            <i className="log-out-icon" onClick={logout}>⇦</i>
+
+        </div>
+    )
+}
+
+export default ChatHeader
